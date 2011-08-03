54603a4e67ea5195bcfe3d41dfbe20bea843357b
Alex Henning <elcron@gmail.com>
2011-08-03 11:15:47 -0400

    Added readme with dependencies.

--- server.py
+++ server.py
@@ -1,13 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 from bottle import run, route, template, view, request
-
-#template("twitter-sentiment-query.html", name="twitter-sentiment-query")
 
 @route("/")
 @view("twitter-sentiment-query.html")
 def twitterSentimentQuery():
     q = request.GET.get("q", "@github, #gitorious, git meetup")
+    queries = [s.strip() for s in q.split(",")]
     return {"q": q}
 
 if __name__ == "__main__":
